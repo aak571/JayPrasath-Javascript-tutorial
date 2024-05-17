2c26423e7dcde044cde0a23a049a5496ff6c1aa2
Aakash <aakash.kavi10@gmail.com>
2024-05-17 14:33:20 +0530

    first commit

---
+++ JS-tutorial/js-practice.js
@@ -0,0 +1,20 @@
+/********************************************JavaScript Statements****************************************/
+
+//    1). Print sum of two numbers
+// let a = 100, b = 200
+// a = 500
+// b = 600
+// console.log(a, b, a + b)
+
+// const cat = 100, dog = 200, d = 100// const - constant
+// let e = 400
+// const f = 800
+// let x = 300
+// console.log('c', 'd', 'f', cat, d, f)
+
+// 2). Strings
+// let jay = 'Jay Prasath'
+// let aak = 'Aakash'
+// const ajith = 'ajith'
+// console.log(jay[2], aak[6], ajith)
+
