59cb0566787bed2d53d5343d256f3c590a89a69d
Aakash <aakash.kavi10@gmail.com>
2024-05-22 17:59:00 +0530

    string-interpolation

---
+++ JS-tutorial/string-interpolation.js
@@ -0,0 +1,15 @@
+// Addition in the any place of a string is called string-interpolation
+
+const jay = 'Jay Prasath '
+
+const str1 = 'is a very good boy !!!'
+
+const height = 5.6
+
+const weight = 100
+
+const str2 = ` he stands ${height} ft`
+
+const str3 = ` and he weighs ${weight} KGs` // string interpolation (back tick `)
+
+console.log(jay + str1 + str2 + str3)
